788c665a825224a14b5dd4479af6c48866a59faa
des <severovostok317@gmail.com>
2026-08-10 19:19:29 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.guard.notifyguard"
         minSdk = 29
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
     }
 
     // Ключ подписи берётся из переменных окружения.
